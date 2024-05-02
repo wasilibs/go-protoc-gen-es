b1ac9aeff4f3ee86012900361b95553f5652b39e
Anuraag Agrawal <anuraaga@gmail.com>
2024-05-02 10:49:55 +0900

    Migrate from mage to goyek

--- protoc_gen_es_test.go
+++ protoc_gen_es_test.go
@@ -9,12 +9,12 @@ import (
 )
 
 func TestBuf(t *testing.T) {
-	if err := os.RemoveAll(filepath.Join("build", "buf")); err != nil {
+	if err := os.RemoveAll(filepath.Join("out", "buf")); err != nil {
 		t.Fatalf("failed to remove build directory: %v", err)
 	}
 
 	output := bytes.Buffer{}
-	cmd := exec.Command("go", "run", "github.com/bufbuild/buf/cmd/buf@v1.30.0", "generate")
+	cmd := exec.Command("go", "run", "github.com/bufbuild/buf/cmd/buf@v1.31.0", "generate")
 	cmd.Stderr = &output
 	cmd.Stdout = &output
 	cmd.Dir = "testdata"
@@ -23,9 +23,9 @@ func TestBuf(t *testing.T) {
 	}
 
 	for _, path := range []string{
-		filepath.Join("build", "buf", "es", "helloworld_pb.d.ts"),
-		filepath.Join("build", "buf", "es", "helloworld_pb.js"),
-		filepath.Join("build", "buf", "ts", "helloworld_pb.ts"),
+		filepath.Join("out", "buf", "es", "helloworld_pb.d.ts"),
+		filepath.Join("out", "buf", "es", "helloworld_pb.js"),
+		filepath.Join("out", "buf", "ts", "helloworld_pb.ts"),
 	} {
 		if _, err := os.Stat(path); err != nil {
 			t.Errorf("failed to stat %v: %v", path, err)
